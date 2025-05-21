46000456c0b5cb02294a9d04c0b5eaf1a50c4f7c
optshj <optshj@gmail.com>
2025-05-21 18:37:19 +0900

    [Feat] 디코 웹봇 추가

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "instance_id" {
-    description = "Instance EC2 ID"
-    value = aws_instance.tf-ec2.id
-}
-output "instance_public_ip" {
-    description = "EC2 Public IP Address"
-    value = aws_instance.tf-ec2.public_ip
-}

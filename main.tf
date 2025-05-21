72a9699e584561b6ac968c8a3cc030e2cb74e3c2
optshj <optshj@gmail.com>
2025-05-21 17:12:19 +0900

    [Fix] Terraform 수정

--- main.tf
+++ main.tf
@@ -2,9 +2,35 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# 기존 보안 그룹 조회
-data "aws_security_group" "react_sg" {
+# 보안 그룹 존재 여부 확인 (data 소스)
+data "aws_security_group" "existing_sg" {
   name = "react-sg"
+}
+
+# 새로운 보안 그룹 생성 (조건부)
+resource "aws_security_group" "new_sg" {
+  count       = length(data.aws_security_group.existing_sg) > 0 ? 0 : 1
+  name        = "react-sg"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 보안 그룹 ID 선택
+locals {
+  security_group_id = length(data.aws_security_group.existing_sg) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.new_sg[0].id
 }
 
 resource "aws_instance" "react_app" {
@@ -23,8 +49,8 @@ resource "aws_instance" "react_app" {
                 ${var.docker_image}
               EOF
 
-  # 기존 보안 그룹 연결
-  vpc_security_group_ids = [data.aws_security_group.react_sg.id]
+  vpc_security_group_ids = [local.security_group_id]
+  tags = "Hunt EC2"
 }
 
 variable "docker_image" {

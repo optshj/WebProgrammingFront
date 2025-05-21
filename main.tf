75ce3ed697dd7e9db1f85875be6358e52de7c3de
optshj <optshj@gmail.com>
2025-05-21 16:42:08 +0900

    [Fix] 보안그룹 수정

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "aws_security_group" "react_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
